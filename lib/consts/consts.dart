88a7bb965e4c441bf9b865b9a4500990609c6891
Ashish-chanchal <akchanchal2002@gmail.com>
2024-05-21 17:31:00 +0530

    code added

--- lib/consts/consts.dart
+++ lib/consts/consts.dart
@@ -8,5 +8,3 @@ export 'package:get/get.dart';
 export './list.dart';
 export 'package:url_launcher/url_launcher.dart';
 
-const OPENAI_API_KEY =
-    'sk-proj-5jm8qFETMI8FEazus4pXT3BlbkFJfWa5XF4HzqtszRC36q59';
